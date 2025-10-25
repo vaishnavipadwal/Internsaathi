0bab3588b8020448e67f7c355d5d87978adbc293
kalash <kalashsanoniya1818@gmail.com>
2025-10-25 11:27:44 +0530

    update env and api file or create docker file

--- frontend/src/api/applicationService.js
+++ frontend/src/api/applicationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/applications/';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/applications/`;
 
 // Helper function to get the user token from local storage
 const getToken = () => {
--- frontend/src/api/collegeService.js
+++ frontend/src/api/collegeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/colleges/';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/applications/`;
 
 // Helper function to get the user token from local storage
 const getToken = () => {
